afe3870661129e70ba39c821041a59a9b2eadab8
Hitokix <arles.edwin@gmail.com>
2017-09-05 00:51:28 -0500

    Add files via upload

--- app/scripts/content.js
+++ app/scripts/content.js
@@ -13,17 +13,6 @@ var blocksName = {
     ]
 };
 
-$(document).ready(function() {
-    $('.datepicker').pickadate({
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 15, // Creates a dropdown of 15 years to control year,
-        today: 'Today',
-        clear: 'Clear',
-        close: 'Ok',
-        closeOnSelect: false // Close upon selecting a date,
-    });
-});
-
 var upContent = document.querySelector('#upContent');
 
 function getStateTitle(state) {
@@ -34,6 +23,35 @@ function getStateTitle(state) {
     }
 }
 
+function levelChangeTable() {
+    var tableLevelInfo;
+    var tableLvl;
+    var level = document.getElementById('selLevel').value;
+
+    if (level === '1') {
+        tableLevelInfo = document.querySelector('#tableLevel');
+        tableLvl = [
+            { 'Tipo': 'Administracion', 'Monto': 'USD 600.00', 'Budget': '1000.00', 'Diff': '400.00' },
+            { 'Tipo': 'Legales', 'Monto': 'USD 1800.00', 'Budget': '2000.00', 'Diff': '200.00' },
+            { 'Tipo': 'Operativos', 'Monto': 'USD 6000.00', 'Budget': '6000.00', 'Diff': '0.00' },
+            { 'Tipo': 'Otros', 'Monto': 'USD 500.00', 'Budget': '400.00', 'Diff': '-100.00' }
+        ];
+        tableLevelInfo.innerHTML = ExpApp.tableLevel_1({ account: tableLvl });
+    } else if (level === '2') {
+        tableLevelInfo = document.querySelector('#tableLevel');
+        tableLvl = [
+            { 'Cuenta': '610112', 'Monto': 'USD 25.00', 'Fecha': '25-08-2017', 'Concepto': 'Gasto de Combustible' },
+            { 'Cuenta': '610115', 'Monto': 'USD 65.00', 'Fecha': '22-08-2017', 'Concepto': 'Internet' },
+            { 'Cuenta': '610111', 'Monto': 'USD 350.00', 'Fecha': '14-08-2017', 'Concepto': 'Mantenimiento de Vehiculos' },
+            { 'Cuenta': '610111', 'Monto': 'USD 1400.00', 'Fecha': '11-08-2017', 'Concepto': 'Mantenimiento de local' },
+            { 'Cuenta': '620001', 'Monto': 'USD 650.00', 'Fecha': '01-08-2017', 'Concepto': 'Vaijes Accionistas' }
+        ];
+
+        tableLevelInfo.innerHTML = ExpApp.tableBase({ account: tableLvl });
+    }
+};
+
+
 function changePageState(state) {
     var upContext = { 'state': state, 'title': getStateTitle(state) };
     upContent.innerHTML = ExpApp.content(upContext);
@@ -43,4 +61,68 @@ function changePageState(state) {
 
     $('.menuLink').removeClass('menuActive');
     $('#' + state + 'Link').addClass('menuActive');
+
+    if (state === 'newExpense') {
+
+        $(document).ready(function() {
+            $('.datepicker').pickadate({
+                selectMonths: true, // Creates a dropdown to control month
+                selectYears: 15, // Creates a dropdown of 15 years to control year,
+                today: 'Today',
+                clear: 'Clear',
+                close: 'Ok',
+                closeOnSelect: false // Close upon selecting a date,
+            });
+        });
+
+        var tableAccount = document.querySelector('#tableAccount');
+        var tableAcct = [
+            { 'Cuenta': '610112', 'Monto': 'USD 25.00', 'Fecha': '25-08-2017', 'Concepto': 'Gasto de Combustible' },
+            { 'Cuenta': '610115', 'Monto': 'USD 65.00', 'Fecha': '22-08-2017', 'Concepto': 'Internet' },
+            { 'Cuenta': '610111', 'Monto': 'USD 350.00', 'Fecha': '14-08-2017', 'Concepto': 'Mantenimiento de Vehiculos' },
+            { 'Cuenta': '610111', 'Monto': 'USD 1400.00', 'Fecha': '11-08-2017', 'Concepto': 'Mantenimiento de local' },
+            { 'Cuenta': '620001', 'Monto': 'USD 650.00', 'Fecha': '01-08-2017', 'Concepto': 'Vaijes Accionistas' }
+        ];
+
+        tableAccount.innerHTML = ExpApp.tableBase({ account: tableAcct });
+    } else if (state === 'expenseMove') {
+        $(document).ready(function() {
+            $('select').material_select();
+        });
+
+    } else if (state === 'expenseForcast') {
+        $(document).ready(function() {
+            $('select').material_select();
+        });
+
+        var sliderMonth = document.getElementById('sliderMonth');
+        noUiSlider.create(sliderMonth, {
+            start: [1, 3],
+            connect: true,
+            tooltips: true,
+            step: 1,
+            format: wNumb({
+                decimals: 1,
+            }),
+            range: { 'min': [1], 'max': [12] }
+
+        });
+
+        var inputFormatMin = document.getElementById('sliderMonthMin');
+        var inputFormatMax = document.getElementById('sliderMonthMax');
+
+        sliderMonth.noUiSlider.on('update', function(values, handle) {
+            inputFormatMin.value = values[0];
+            inputFormatMax.value = values[1];
+        });
+
+        var tableForeCast = document.querySelector('#tableForeCast');
+        var tableFor = [
+            { 'Tipo': 'Administracion', 'Monto': 'USD 600.00', 'Budget': '1000.00', 'ForeCast': '500.00' },
+            { 'Tipo': 'Legales', 'Monto': 'USD 1800.00', 'Budget': '2000.00', 'ForeCast': '300.00' },
+            { 'Tipo': 'Operativos', 'Monto': 'USD 6000.00', 'Budget': '6000.00', 'ForeCast': '20.00' },
+            { 'Tipo': 'Otros', 'Monto': 'USD 500.00', 'Budget': '400.00', 'ForeCast': '600.00' }
+        ];
+        tableForeCast.innerHTML = ExpApp.tableForeCast({ account: tableFor });
+    }
 };
